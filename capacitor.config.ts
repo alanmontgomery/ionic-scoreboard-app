7cdcdb43bd7744271960d2814ab72999900ee344
Alan Montgomery <alan.montgomery@tascomi.com>
2021-10-05 15:16:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-react-scores',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
